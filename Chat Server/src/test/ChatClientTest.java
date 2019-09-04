a3893cbf2230bb9653529f7ed1447fe696bbc301
Ivan Spasov <johnspasov99@gmail.com>
2019-09-04 10:37:14 +0300

    ChatServer

---
+++ Chat Server/src/test/ChatClientTest.java
@@ -0,0 +1,9 @@
+import bg.sofia.uni.fmi.mjt.chat.ChatClient;
+import bg.sofia.uni.fmi.mjt.chat.ChatServer;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+public class ChatClientTest {
+}
